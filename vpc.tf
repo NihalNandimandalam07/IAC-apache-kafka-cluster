9bf2e9aebe0ac29ed10962ce686c399d48af1487
NihalNandimandalam07 <nnandimandalam@verticalrelevance.com>
2026-09-22 11:28:06 -0400

    adding terraform files to the repo

---
+++ vpc.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "kafka-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "kafka-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "kafka-igw" {
+  vpc_id = aws_vpc.kafka-vpc.id
+
+  tags = {
+    Name = "kafka-igw"
+  }
+}
+
+resource "aws_subnet" "kafka-subnet" {
+  count = length(var.availability_zones)
+  vpc_id     = aws_vpc.kafka-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "kafka-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "kafka-route-table" {
+  vpc_id     = aws_vpc.kafka-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kafka-igw.id
+  }
+
+  tags = {
+    Name = "kafka-route-table"
+  }
+}
+
+resource "aws_route_table_association" "kafka-route-table-association" {
+  count = length(var.availability_zones)  
+  subnet_id      = aws_subnet.kafka-subnet[count.index].id
+  route_table_id = aws_route_table.kafka-route-table.id
+}
