81027f1ac0d9620fa60f39901b51495bc665c5ec
NihalNandimandalam07 <nnandimandalam@verticalrelevance.com>
2026-09-22 22:05:32 -0400

    Uncomment IAM role policy attachment for SSM
    Uncommented the aws_iam_role_policy_attachment resource for SSM policy.

--- iam.tf
+++ iam.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "kafka_role" {
 
 }
 
-/*resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
   role       = aws_iam_role.kafka_role.name
   policy_arn = "arn:aws:iam::policy/AmazonSSMManagedInstanceCore"
 } 
@@ -24,4 +24,4 @@ resource "aws_iam_role" "kafka_role" {
 resource "aws_iam_instance_profile" "kafka_instance_profile" {
   name_prefix = "kafka-"
   role        = aws_iam_role.kafka_role.name
-} */
+}
