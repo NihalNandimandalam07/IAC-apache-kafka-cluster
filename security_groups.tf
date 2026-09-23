2fdf795e8fd95d0bf291ea418458f6ed3917066c
NihalNandimandalam07 <nnandimandalam@verticalrelevance.com>
2026-09-22 21:05:20 -0400

    changes

--- security_groups.tf
+++ security_groups.tf
@@ -1,6 +1,5 @@
 locals{
-    effective_client_cidrs = 
-        length(var.client_access_cidrs) > 0 ? var.client_cidrs : [var.vpc_cidr]
+    effective_client_cidrs = length(var.client_access_cidrs) > 0 ? var.client_cidrs : [var.vpc_cidr]
 }
 
 resource "aws_security_group" "kafka-sg" {
